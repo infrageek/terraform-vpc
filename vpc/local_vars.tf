8806b0917fc5725714b0f05f4905769aae82b733
Trijit Acharyya Chowdhury <trijit@gmail.com>
2019-02-24 23:30:34 +0530

    working state

---
+++ vpc/local_vars.tf
@@ -0,0 +1,29 @@
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+variable "global_tags" {
+  description = "AWS tags that will be added to all resources managed herein"
+  type = "map"
+  default = {
+    "Creator" = "Trijit"
+    "Managed By" = "Terraform"
+  }
+}
+variable "cidr_block" {}
+
+variable "aws_vpc_name" {
+  default = "trijit-terraform-test"
+}
+variable "vpc_instance_tenancy" {
+  default = "default"
+}
+
+variable "vpc_enable_dns_support" {
+  default = "true"
+}
+
+variable "vpc_enable_dns_hostnames" {
+  default = "true"
+}
---
+++ vpc/vpc.tf
@@ -0,0 +1,7 @@
+resource "aws_vpc" "VPC_Creation" {
+  cidr_block           = "${var.cidr_block}"
+  enable_dns_support = "${var.vpc_enable_dns_support}"
+  enable_dns_hostnames = "${var.vpc_enable_dns_hostnames}"
+  instance_tenancy = "${var.vpc_instance_tenancy}"
+  tags = "${merge(var.global_tags, map("Name", "${var.aws_vpc_name}"))}"
+}
